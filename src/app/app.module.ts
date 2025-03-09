3beb199a632fd4bfa796cbfe87aedfffaa98010d
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-03-09 15:24:09 +0300

    Implement task management with NgRx store and enhance UI layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
-import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { taskReducer } from './store/task.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forRoot({ tasks: taskReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: false }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/task.reducer.ts
@@ -0,0 +1,35 @@
+import { createReducer, on } from '@ngrx/store';
+import { addTask, deleteTask, completeTask } from './task.actions';
+
+export interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
+export interface TaskState {
+  tasks: Task[];
+}
+
+export const initialState: TaskState = {
+  tasks: []
+};
+
+export const taskReducer = createReducer(
+  initialState,
+  on(addTask, (state, { task }) => ({ ...state, tasks: [...state.tasks, task] })),
+  on(completeTask, (state, { id }) => ({
+    ...state,
+    tasks: state.tasks.map(task => task.id === id ? { ...task, completed: true } : task)
+  })),
+  on(deleteTask, (state, { id }) => ({
+    ...state,
+    tasks: state.tasks.filter(task => task.id !== id)
+  }))
+);
+
+// Selectors
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+
+export const selectTaskState = createFeatureSelector<TaskState>('tasks');
+export const selectAllTasks = createSelector(selectTaskState, (state) => state.tasks);
